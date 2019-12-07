3b2c374196824f0a0e4d812024baacc699dd6fe5
toyooka.yuto@baroque-w.jp <toyooka.yuto@baroque-w.jp>
2019-12-08 08:36:22 +0900

    #保存システム

--- main.js
+++ main.js
@@ -19,9 +19,42 @@ var todoStorage = {
 const app = new Vue({
     el: '#app',
     data: {
-      // 使用するデータ
+        todos: []
     },
     methods: {
-      // 使用するメソッド
-    }
+        // ToDo 追加の処理
+        doAdd: function(event, value) {
+          // ref で名前を付けておいた要素を参照
+          var comment = this.$refs.comment
+          // 入力がなければ何もしないで return
+          if (!comment.value.length) {
+            return
+          }
+          // { 新しいID, コメント, 作業状態 }
+          // というオブジェクトを現在の todos リストへ push
+          // 作業状態「state」はデフォルト「作業中=0」で作成
+          this.todos.push({
+            id: todoStorage.uid++,
+            comment: comment.value,
+            state: 0
+          })
+          // フォーム要素を空にする
+          comment.value = ''
+        }
+    },
+    watch: {
+        // オプションを使う場合はオブジェクト形式にする
+        todos: {
+          // 引数はウォッチしているプロパティの変更後の値
+          handler: function(todos) {
+            todoStorage.save(todos)
+          },
+          // deep オプションでネストしているデータも監視できる
+          deep: true
+        }
+    },
+    created() {
+        // インスタンス作成時に自動的に fetch() する
+        this.todos = todoStorage.fetch()
+      }
   })
